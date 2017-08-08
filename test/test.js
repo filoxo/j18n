0005b17393893c2d32b87878a006ba3f38be9d64
Carlos Filoteo <cfiloteo@ancestry.com>
2017-08-08 15:31:40 -0600

    Add 'create flat file' test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const assert = require('assert'),
     { checkOutput, lastLine } = require('./utils'),
+    { existsSync, readFileSync, unlink } = require('fs'),
     { exec } = require('child_process')
 
 describe('j18n', () => {
@@ -18,6 +19,24 @@ describe('j18n', () => {
       exec('j18n flat', (error, stdout, stderr) => {
         assert.equal(lastLine(stderr), e)
         done()
+      })
+    })
+
+    it('should create flat file', done => {
+      const outputFile = 'test/nested.flat.json'
+      exec('j18n flat -f test/nested.json', (error, stdout, stderr) => {
+        const expected = `{
+  "title": "title",
+  "user.name": "Test",
+  "user.id": "123abc",
+  "step.1.desc": "desc",
+  "step.1.heading": "heading"
+}`
+        assert.equal(stdout, 'Data written successfully!\n')
+        assert.ok(existsSync(outputFile))
+        assert.equal(readFileSync(outputFile, 'utf8'), expected)
+        // Cleanup
+        unlink(outputFile, done)
       })
     })
   });
