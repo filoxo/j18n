baafc2ed02ac1fb546810f53b58e588e503443ea
Carlos Filoteo <carlosfiloteo@canopytax.com>
2018-06-07 20:56:02 -0600

    Replace nest, flat functions from main

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+const { nest, flat, getSaveToFilename } = require('./main.js')
+
 const { argv } = require('yargs')
     .usage('Usage: j18n nest -f [file]')
     .command('flat', 'Flatten json key structures')
@@ -64,41 +66,3 @@ function main() {
   })
 }
 
-function nest(data) {
-  if (Object(data) !== data || Array.isArray(data)) return data
-  const regex = /\.?([^.\[\]]+)|\[(\d+)\]/g
-  let res = {}
-  for (var p in data) {
-    let cur = res,
-      prop = '',
-      m
-    while ((m = regex.exec(p))) {
-      cur = cur[prop] || (cur[prop] = m[2] ? [] : {})
-      prop = m[2] || m[1]
-    }
-    cur[prop] = data[p]
-  }
-  return res[''] || res
-}
-
-function flat(data) {
-  let res = {}
-  function f(cur, prop) {
-    if (Object(cur) !== cur) {
-      res[prop] = cur
-    } else if (Array.isArray(cur)) {
-      for (let i = 0, l = cur.length; i < l; i++)
-        f(cur[i], prop + '[' + i + ']')
-      if (l == 0) res[prop] = []
-    } else {
-      var isEmpty = true
-      for (var p in cur) {
-        isEmpty = false
-        f(cur[p], prop ? prop + '.' + p : p)
-      }
-      if (isEmpty && prop) res[prop] = {}
-    }
-  }
-  f(data, '')
-  return res
-}
